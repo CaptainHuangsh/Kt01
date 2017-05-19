41d53fea1e6683dee43cc91762a098dc34a35dc6
huangshaohua <huangshaohuamail@gmail.com>
2017-05-19 20:43:44 +0800

    Initial commit

---
+++ src/cn/captainhuang/kt01/Main.kt
@@ -0,0 +1,11 @@
+package cn.captainhuang.kt01
+
+/**
+ * Created by owen on 2017/5/19.
+ */
+fun main(args: Array<out String>) {
+    print("hello world")
+    print(Main(0,"huang"))
+}
+
+data class Main(var id:Int,var name:String)
